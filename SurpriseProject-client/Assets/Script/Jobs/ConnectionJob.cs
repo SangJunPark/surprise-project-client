63db40ebb416dbc48d1933f4d09664c51c4a9883
stzSangjunPark <sangjun.park@sundaytoz.com>
2018-12-21 02:12:18 +0900

    jun - probuilder & initial gamelogic

---
+++ SurpriseProject-client/Assets/Script/Jobs/ConnectionJob.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+struct IncrementByDeltaTimeJob : IJobParallelFor
+{
+    public NativeArray<float> values;
+    public float deltaTime;
+
+    public void Execute(int index)
+    {
+        float temp = values[index];
+        temp += deltaTime;
+        values[index] = temp;
+    }
+}
