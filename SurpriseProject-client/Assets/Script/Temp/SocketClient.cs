98fcd5b303ffb03054214645599a52c7e47f87f1
d <dantzchan@gmail.com>
2019-01-16 22:36:45 +0900

    로컬에서 연결 되게 수정

--- SurpriseProject-client/Assets/Script/Temp/SocketClient.cs
+++ SurpriseProject-client/Assets/Script/Temp/SocketClient.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using System.IO;
+
 
 public class SocketClient : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private byte[] _receiveBuffer = new byte[8142];
+    public static string response;
+
     void Start()
     {
-        
+        SetupServer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetupServer()
     {
-        
+        try
+        {
+            _clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 3000));
+            Debug.Log("connected to " + IPAddress.Loopback);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+        _clientSocket.BeginReceive(_receiveBuffer, 0, _receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+
+    }
+
+    private void ReceiveCallback(IAsyncResult AR)
+    {
+        int received = _clientSocket.EndReceive(AR);
+        if (received <= 0)
+            return;
+
+        byte[] recData = new byte[received];
+        Buffer.BlockCopy(_receiveBuffer, 0, recData, 0, received);
+
+        response = Encoding.Default.GetString(recData);
+
+        Debug.Log(response);
+
+        _clientSocket.BeginReceive(_receiveBuffer, 0, _receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+    }
+
+    private void SendData(byte[] data)
+    {
+        SocketAsyncEventArgs socketAsyncData = new SocketAsyncEventArgs();
+        socketAsyncData.SetBuffer(data, 0, data.Length);
+        _clientSocket.SendAsync(socketAsyncData);
     }
 }
